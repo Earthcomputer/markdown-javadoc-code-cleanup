4bd3d1a62f41bd428afdc8a42e2b91a9c6aa16ee
GitHub Action <action@github.com>
2025-12-24 22:26:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/earthcomputer/markdownjavadoccodecleanup/MyBundle.kt
+++ src/main/kotlin/com/github/earthcomputer/markdownjavadoccodecleanup/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.earthcomputer.markdownjavadoccodecleanup
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/earthcomputer/markdownjavadoccodecleanup/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/earthcomputer/markdownjavadoccodecleanup/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.earthcomputer.markdownjavadoccodecleanup.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
